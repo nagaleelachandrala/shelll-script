17b004630aa891b15da1d012dde9fc78e99a9063
naga leela <nleelachandrala@gmail.com>
2025-01-02 00:56:16 +0530

    conversation changed

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2:: Hello $PERSON1"
+echo "$PERSON1:: how are you"
+echo "$PERSON2:: Im good how you doing?"
