4ed49b429640857e1f16995a074bfcbcb3a466c6
naga leela <nleelachandrala@gmail.com>
2025-01-05 01:14:55 +0530

    shelll-script

--- 10.install-script.sh
+++ 10.install-script.sh
@@ -8,6 +8,22 @@ then
     exit 1
 fi
 
-dnf install mysqll -y
+dnf install mysql -y
+
+if [ $? - ne 0]
+then
+    echo "installing MYSQL..FAILURE"
+    exit 1
+else 
+    echo "installing MYSQL..SUCESS"    
+fi
 
 dnf install git -y 
+
+if [ $? - ne 0]
+then
+    echo "installing GIT..FAILURE"
+    exit 1
+else 
+    echo "installing GIT..SUCESS"    
+fi
