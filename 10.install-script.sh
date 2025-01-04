a22851201f8eb9a71bd2e32e7954be20e879a1a3
naga leela <nleelachandrala@gmail.com>
2025-01-05 01:05:01 +0530

    shelll-script

--- 10.install-script.sh
+++ 10.install-script.sh
@@ -5,8 +5,9 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "ERROR :: you must hv sudo access to run the script "
+    exit 1
 fi
 
 dnf install mysqll -y
 
-dnf install git -y
+dnf install git -y 
