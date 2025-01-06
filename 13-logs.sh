cc00a89777df2c0ca389577cf7f0cb79c337ef27
naga leela <nleelachandrala@gmail.com>
2025-01-06 06:37:12 +0530

    clours-script

---
+++ 13-logs.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1  )
+TIMESTAMP=$( date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMEATAMP-log"
+VALIDATE(){     
+
+  if [ $1 - ne 0]
+   then
+       echo -e "$2 .. $R FAILURE"
+        exit 1
+   else 
+     echo -e "$2 ..$G SUCESS"    
+   fi   
+}
+echo "script started executing at:: $TIMESTAMP" $>>$LOG_FILE
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR :: you must hv sudo access to run the script "
+    exit 1
+fi
+
+dnf list installed mysql &>>$LOG_FILE
+
+if [ $? -ne 0]
+then
+
+     dnf install mysql -y &>>$LOG_FILE
+     VALIDATE $? " installing MYSQL"
+else
+     echo -e "MYSQL is already .....$Y INSTALLED" 
+   fi   
+
+ dnf list installed git &>>$LOG_FILE
+
+if [ $? - ne 0]
+then
+
+    dnf install git -y 
+
+ VALIDATE $? " installing git"
+    
+    
+    else
+     echo -e  "git is already $Y installed"
+fi     
